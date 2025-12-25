bb74e0303098139387bd22e7a8ca6ce6ca9e934d
kiruthika1321 <kiruthikasivalingam13@gmail.com>
2025-12-25 20:45:22 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,1 @@
+print("This is for the Big project I am planning for a Retail SAAS software, I am looking for support from GitHub community, Interested people can comment/intiate a pull request etc.")
